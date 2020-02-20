d18ee48c949740b0ba3ba1697208ba7d6db2638d
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-02-20 14:44:28 +0900

    🍐 feat: add components/ListItem.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import ListItem from './components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -38,22 +39,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/id/10/100/100' }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi u
-          </Text>
-          <Text style={styles.subText}>ReactNews</Text>
-        </View>
-      </View>
+      <ListItem />
     </View>
   );
 }
