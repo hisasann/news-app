059771584bd6f3a03cd0c3f30703cbf47b4b6e8b
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-03-27 18:40:06 +0900

    🍴 feat: remove connect of redux, add hooks for redux

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import {
-  View,
   SafeAreaView,
   StyleSheet,
-  Text,
-  TouchableOpacity,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
-// react-native-web-community/react-native-web-webview: React Native for Web implementation of RN's WebView - https://github.com/react-native-web-community/react-native-web-webview
-// import WebView from 'react-native-web-webview';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { addClip, deleteClip } from '../store/actions/user';
 import ClipButton from '../components/ClipButton';
 
@@ -20,37 +15,35 @@ const styles = StyleSheet.create({
   },
 });
 
-const ArticleScreen = props => {
+const ArticleScreen = ({ route, navigation }) => {
   const [url, setUrl] = useState();
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const article = props.navigation.getParam('article');
+    const { article } = route.params;
+    // const article = props.navigation.getParam('article');
     setUrl(article.url);
   }, []);
 
   const isClipppd = () => {
-    const article = props.navigation.getParam('article');
-    return props.user.clips.some(clip => clip.url === article.url);
+    const { article } = route.params;
+    // const article = props.navigation.getParam('article');
+    return user.clips.some((clip) => clip.url === article.url);
   };
 
   const toggleClip = () => {
+    const { article } = route.params;
     if (isClipppd()) {
-      props.deleteClip({ clip: props.navigation.getParam('article') });
+      dispatch(deleteClip({ clip: article }));
     } else {
-      props.addClip({ clip: props.navigation.getParam('article') });
+      dispatch(addClip({ clip: article }));
     }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <ClipButton
-        onPress={toggleClip}
-        enabled={isClipppd()}
-      />
-      {/*
-        おそらく iframe で表示されるため、 sameorigin じゃないのでエラーが履かれてしまう
-        Refused to display 'https://headlines.yahoo.co.jp/hl?a=20200305-00000091-mai-int' in a frame because it set 'X-Frame-Options' to 'sameorigin'
-      */}
+      <ClipButton onPress={toggleClip} enabled={isClipppd()} />
       <WebView
         // style={{width: 300, height: 300}}
         originWhitelist={['*']}
@@ -61,11 +54,12 @@ const ArticleScreen = props => {
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    user: state.user,
-  };
-};
-const mapDispatchToProps = { addClip, deleteClip };
-
-export default connect(mapStateToProps, mapDispatchToProps)(ArticleScreen);
+// const mapStateToProps = (state) => {
+//   return {
+//     user: state.user,
+//   };
+// };
+// const mapDispatchToProps = { addClip, deleteClip };
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(ArticleScreen);
+export default ArticleScreen;
